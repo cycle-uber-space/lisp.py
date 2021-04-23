3f9e4b234766e2e6b365af974a7cccae0ba4cb07
Cycle <cycle@uber.space>
2021-04-23 13:10:28 +0200

    add representation for nil

--- lisp.py
+++ lisp.py
@@ -7,6 +7,19 @@ py_list = list
 py_type = type
 py_repr = repr
 
+class Nil:
+    def __iter__(self):
+        return ListIter(self)
+    def __repr__(self):
+        return "nil"
+    def __len__(self):
+        return 0
+
+nil = Nil()
+
+def is_nil(exp):
+    return isinstance(exp, Nil)
+
 def main(argc, argv):
     pass
 
