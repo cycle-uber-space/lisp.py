c963e003f0e209e0d58b95d1e34820da424a37db
Cycle <cycle@uber.space>
2021-04-26 17:00:32 +0200

    extend standard library

--- std.lisp
+++ std.lisp
@@ -1,2 +1,18 @@
 
 (def pi 3.141592654)
+
+(defun not (arg)
+  (if arg
+      nil
+      t))
+
+(defmacro progn body
+  ;; TODO use backquote
+  ;;`(lambda () ,@body)
+  (cons (cons 'lambda (cons '() body)) nil))
+
+(defmacro when (test . body)
+  (cons 'if (cons test (cons (cons 'progn body) nil))))
+
+(defmacro unless (test . body)
+  (cons 'when (cons (cons 'not (cons test nil)) body)))
